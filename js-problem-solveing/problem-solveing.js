3d87c2cf0b4097fe011f604913b76aabe19fd9be
TFMahabub <rjmahabub543@gmail.com>
2023-01-20 10:31:20 +0600

    3no problem solved

--- js-problem-solveing/problem-solveing.js
+++ js-problem-solveing/problem-solveing.js
@@ -10,6 +10,13 @@
 // console.log(!!undefined);
 // console.log(!!1);
 // console.log(Boolean(undefined));
-console.log(Boolean("abc"));
+// console.log(Boolean("abc"));
 
-// 3.
+// 3. Resize any array:-
+// const animal = ["dog", "cat", "moncky", "lion"];
+// animal.length = 3;
+// console.log(animal);
+
+// 4. How to flattern a multi dimensional array:-
+const array = [0, 2, 3, 4, 5, 6, ["name", 2], [0, 1], "mahabub"];
+console.log(array.flat());
