00ea391fbccc0f860b0fffe8a3b64359c30b63b0
TFMahabub <rjmahabub543@gmail.com>
2023-01-20 10:19:34 +0600

    first commit

---
+++ js-problem-solveing/problem-solveing.js
@@ -0,0 +1,9 @@
+// 1. remove falsey value from an array:-
+// const array = [1, 3, 0, false, true, null, undefined, []];
+
+// const result = array.filter(Boolean);
+// console.log(result);
+
+// 2. convert any value to boolean:-
+console.log(!!"mahabub");
+console.log(!!0);
